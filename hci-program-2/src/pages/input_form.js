af35f743bb2ea5155c1a0857b940b1da96c3c200
jacarp <91710233+jacarp@users.noreply.github.com>
2022-09-28 12:52:34 -0500

    Started adding to input form

--- hci-program-2/src/pages/input_form.js
+++ hci-program-2/src/pages/input_form.js
@@ -1,13 +1,53 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 const InputForm = () => {
 return (
 	<div>
-	<h1>
-		Input form page.
-	</h1>
+		<h1>
+			Submit a Ticket
+		</h1>
+
+		<div>
+			<form>
+				<label>
+					Name:
+					<input type="text" name="name" />
+				</label>
+			</form>
+		</div>
+
+		<div>
+			<form>
+				<label>
+					Email:
+					<input type="text" name="email" />
+				</label>
+			</form>
+		</div>
+
+		<div>
+			<form>
+				<label>
+					Email:
+					<input type="text" name="email" />
+				</label>
+			</form>
+		</div>
+
+		<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+			<a class="dropdown-item">Technology Issue</a>
+			<a class="dropdown-item">Device Request</a>
+		</div>
+		<button type="button" class="btn btn-success" href="">
+			Submit Ticket
+			
+		</button>
+
 	</div>
 );
 };
 
 export default InputForm;
+
+/*<Route path='/home' element={<Home />} />*/
