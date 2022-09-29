556630a3b8e3120a8a6a19c7e505565488241de9
brlewi03 <brlewi03@wsc.edu>
2022-09-29 18:40:33 -0500

    Added Comments to App.js about canvas questions

--- hci-program-2/src/App.js
+++ hci-program-2/src/App.js
@@ -7,6 +7,12 @@ import InputForm from './pages/input_form';
 import ViewForm from './pages/view_form';
 import Footer from './components/Footer';
 
+// Questions on Canvas 
+//1. Who is our audience? The employees at the company that is using the ticket system. 
+//2. What are the goals for the user? They want to request help with their device or various devices.
+//3. How did we understand and document context and goals?
+//4. Can We/Did We use any of the cognitive/behavior patterns discussed in the textbook? 
+
 function App() {
 return (
     <div>
