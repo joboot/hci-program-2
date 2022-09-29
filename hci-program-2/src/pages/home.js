e7c9763fb7fe2d7228c333372c7335c8b4b1a159
joboot <jordan.booth41900@gmail.com>
2022-09-29 17:06:22 -0500

    Added table with test data

--- hci-program-2/src/pages/home.js
+++ hci-program-2/src/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Table} from "react-bootstrap"
 
 const Home = () => {
 return (
@@ -7,6 +8,28 @@ return (
 		Home page.
 	</h1>
 		<button type="button" class="btn btn-primary"><a href="input_form.js">Add Ticket</a></button>
+		<Table striped bordered hover>
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Subject</th>
+					<th>Type</th>
+					<th>Name</th>
+					<th>Phone</th>
+					<th>Date</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th>1</th>
+					<th>PC wont boot</th>
+					<th>Problem</th>
+					<th>Jordan Booth</th>
+					<th>3056644029</th>
+					<th>9/29/2022</th>
+				</tr>
+			</tbody>
+		</Table>
 	</div>
 );
 };
