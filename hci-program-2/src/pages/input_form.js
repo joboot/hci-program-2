0d158c67f45fe2536c1be1391754993899130c5e
jacarp <91710233+jacarp@users.noreply.github.com>
2022-09-29 20:05:26 -0500

    fixed submit button & other stuff

--- hci-program-2/src/pages/input_form.js
+++ hci-program-2/src/pages/input_form.js
@@ -17,7 +17,7 @@ const InputForm = () => {
 	const onSubmit = () => {
 		routeChange()
 		console.log("Submitted ticket");
-	  };
+	};
 
 	return (
 		<div>
@@ -52,12 +52,20 @@ const InputForm = () => {
 				</form>
 			</div>
 
+			<div id="form_item" class="d-flex justify-content-center">
+				<form>
+					<label>
+						Subject:
+						<input type="text" name="email" />
+					</label>
+				</form>
+			</div>
+
 			<div class="btn-group d-flex justify-content-center" id="form_item">
 				<div class="dropdown show">
-					<a class="btn btn-info dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<Button class="btn btn-info dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						Select a Category
-					</a>
-
+					</Button>
 					<div class="dropdown-menu" aria-labelledby="dropdownMenuLink" >
 						<a class="dropdown-item" href="#">Technology Issue</a>
 						<a class="dropdown-item" href="#">Device Request</a>
@@ -65,8 +73,8 @@ const InputForm = () => {
 				</div>
 			</div>
 
-			<div id="form_item">
-				<Button type="button" class="btn btn-success d-flex justify-content-center" onClick={routeChange}>
+			<div id="form_item" class="d-flex justify-content-center">
+				<Button type="button" class="btn btn-primary" onClick={routeChange}>
 					Submit
 				</Button>
 			</div>
