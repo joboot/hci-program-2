87275884bf6cc03f7d09d3875bd3a866fbab09eb
joboot <jordan.booth41900@gmail.com>
2022-10-02 17:27:39 -0500

    Getting input from input ticket form into json obj

--- hci-program-2/src/pages/input_form.js
+++ hci-program-2/src/pages/input_form.js
@@ -1,175 +1,151 @@
-import { React } from "react";
+import  React, { Component } from "react";
 import { Button } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
-import { Dropdown } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
 import { tickets } from '../data/tickets'
-import { type } from "@testing-library/user-event/dist/type";
 
-const InputForm = () => {
-	let navigate = useNavigate();
-	const routeChange = () =>{
-		let nextID = tickets.length
-		console.log(nextID)
+export default class InputForm extends Component {
+	state = {
+		name: "",
+		phone:"",
+		type:"",
+		subject:"",
+		description:""
+	};
 
+	routeChange = () =>{
+		const navigate = useNavigate();
+		
 		let path = '../home'
 		navigate(path);
 	}
 
-	const handleOnChange = e =>{
-		const {name, value} = e.target
-	}
+	// handleOnChange = e =>{
+	// 	const {name, value} = e.target
+	// }
 	
-	const onSubmit = () => {
+	onSubmit = () => {
 		//put save state here
-		routeChange()
+		console.log(this.state);
+		let nextID = tickets.length + 1;
+		console.log(nextID);
 		console.log("Submitted ticket");
-		JSON.stringify({ "id": "nextID", "subject": "subject", "type": "type", "name": "name", "phone": "phone", "date": "date" })
+		let jsonTicket = JSON.stringify({ "id": nextID, "subject": this.state.subject, "type": this.state.type, "name": this.state.name, "phone": this.state.phone, "date": this.state.date });
+		tickets.push(jsonTicket);
+		console.log(tickets)
+		//this.routeChange();
+		
 	};
 
-	return (
+	createFormGroups = () => {
+		return(
+			<div>
+				<Form.Group>
+					<Form.Label>
+					{/* <Form.Label id="form_item" class="d-flex justify-content-center" > */}
+						Name
+					</Form.Label>
+					<Form.Control id="form_item" className="d-flex justify-content-center" 
+						name="name"
+						placeholder="Name"
+						value={this.state.name}
+						onChange={e => this.setState({ name: e.target.value })}
+						type="text"
+						required
+					/>
+				</Form.Group>
+
+				<br />
+
+				<Form.Group>
+					<Form.Label>
+						Phone Number
+					</Form.Label>
+					<Form.Control
+						name="phone"
+						placeholder="Phone Number"
+						value={this.state.phone}
+						onChange={e => this.setState({ phone: e.target.value })}
+						type="text"
+						required
+					/>
+				</Form.Group>
+
+				<br />
+
+				<Form.Group>
+					<Form.Label>
+						Type
+					</Form.Label>
+					<Form.Select
+						value={this.state.type}
+						onChange={e => this.setState({ type: e.target.value })}>
+
+						<option value="" isinvalid="true">
+							Select Type
+						</option>
+						<option value="incident">Incident</option>
+						<option value="request">Request</option>
+
+					</Form.Select>
+				</Form.Group>
+
+				<br />
+
+				<Form.Group>
+					<Form.Label>
+						Subject
+					</Form.Label>
+					<Form.Control
+						name="subject"
+						placeholder="Subject"
+						value={this.state.subject}
+						onChange={e => this.setState({ subject: e.target.value })}
+						type="text"
+						required
+					/>
+				</Form.Group>
+
+				<br />
+
+				<Form.Group>
+					<Form.Label>
+						Description
+					</Form.Label>
+					<Form.Control
+						name="description"
+						placeholder="Description"
+						value={this.state.description}
+						onChange={e => this.setState({ description: e.target.value })}
+						type="text"
+						required
+					/>
+				</Form.Group>
+
+				<br />
+
+			</div>
+		)
+	}
+
+	render() {
+		return (
 		<div>
 			<h1>
 				Submit a Ticket
 			</h1>
+			
+			<Form autoComplete="off" onSubmit={this.onSubmit}>
 
-			<Form autocomplete="off" onSubmit={onSubmit}>
-				<Form.Label id="form_item" class="d-flex justify-content-center" >
-           			Name
-          		</Form.Label>
-				<Form.Control id="form_item" class="d-flex justify-content-center" 
-					name="name"
-					//value={frmData.name}
-					onChange={handleOnChange}
-					placeholder="Name"
-					required
-				/>
+				{this.createFormGroups()};
 
-				<Form.Label>
-           			Phone Number
-          		</Form.Label>
-				<Form.Control
-					name="phone"
-					//value={frmData.phone}
-					onChange={handleOnChange}
-					placeholder="Phone Number"
-				/>
-
-				<Form.Label>
-           			Subject
-          		</Form.Label>
-				<Form.Control
-					name="Subject"
-					//value={frmData.Subject}
-					onChange={handleOnChange}
-					placeholder="Subject"
-					required
-				/>
-
-				<Form.Label>
-           			Description
-          		</Form.Label>
-				<Form.Control
-					name="Description"
-					//value={frmData.desc}
-					onChange={handleOnChange}
-					placeholder="Description"
-					required
-				/>
-
-				<div class="btn-group d-flex justify-content-center" id="form_item">
-				<Dropdown>
-					<Dropdown.Toggle variant="success" id="dropdown-basic">
-						Select a Category
-					</Dropdown.Toggle>
-
-					<Dropdown.Menu name = "type">
-						<Dropdown.Item href="#/action-1">Tech Issue</Dropdown.Item>
-						<Dropdown.Item href="#/action-2">Device Request</Dropdown.Item>
-					</Dropdown.Menu>
-				</Dropdown>
-				</div>
-
-				<Button type="button submit" class="btn btn-primary" onClick={routeChange}>
+				<Button type="button" className="btn btn-primary" onClick={this.onSubmit}>
 					Submit
 				</Button>
 			</Form>
 
-			<div id="form_item" class="d-flex justify-content-center">
-				<Button type="button" class="btn btn-primary" onClick={routeChange}>
-					Submit
-				</Button>
-			</div>
-
 		</div>
-		/*
-		<div>
-			<h1>
-				Submit a Ticket
-			</h1>
-		
-			<div id="form_item" class="d-flex justify-content-center">
-				<Form>
-					<label>
-						Name:  
-						<input type="text" name="name" />
-					</label>
-				</Form>
-			</div>
-
-			<div id="form_item" class="d-flex justify-content-center">
-				<Form>
-					<label>
-						Phone:  
-						<input type="text" name="phone" />
-					</label>
-				</Form>
-			</div>
-
-			<div id="form_item" class="d-flex justify-content-center">
-				<Form>
-					<label>
-						Subject:  
-						<input type="text" name="subject" />
-					</label>
-				</Form>
-			</div>
-
-			<div id="form_item" class="d-flex justify-content-center">
-				<Form>
-					<label>
-						Description:  
-						<input type="text" name="desc" />
-					</label>
-				</Form>
-			</div>
-	
-			<div class="btn-group d-flex justify-content-center" id="form_item">
-				<Dropdown>
-					<Dropdown.Toggle variant="success" id="dropdown-basic">
-						Select a Category
-					</Dropdown.Toggle>
-
-					<Dropdown.Menu name = "type">
-						<Dropdown.Item href="#/action-1">Tech Issue</Dropdown.Item>
-						<Dropdown.Item href="#/action-2">Device Request</Dropdown.Item>
-					</Dropdown.Menu>
-				</Dropdown>
-			</div>
-			
-
-			<div id="form_item" class="d-flex justify-content-center">
-				<Button type="button" class="btn btn-primary" onClick={routeChange}>
-					Submit
-				</Button>
-			</div>
-
-		</div>
-		*/
-	);
+		);
+	}
 	// This is the code for the input form where you can type in your name, phone, subject, and description on the webpage. Also includes buttons via bootstrap that a user can click on.
 };
-
-export default InputForm;
-
-/*<Route path='/home' element={<Home />} />*/
