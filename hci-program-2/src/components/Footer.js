9d497a0b4099b48ffda998897024efe80651af1e
joboot <jordan.booth41900@gmail.com>
2022-10-02 15:35:06 -0500

    Changed footer and ticket info

--- hci-program-2/src/components/Footer.js
+++ hci-program-2/src/components/Footer.js
@@ -4,9 +4,10 @@ const Footer = () => {
     return (
         <footer>
             <ul className='ul'>
-                <li>Contact Info: (402) 804-2507</li>
-                <li>3716 Gordon Tunnel, Apt. 711, 21683, Ralphfurt, Minnesota, United States</li>
-                <li>Morton_Stokes@yahoo.com</li>
+                <li>Contact Info:</li>
+                <li>(402) 804-2507</li>
+                <li>3716 Gordon Tunnel, 21683, Ralphfurt, Minnesota, United States</li>
+                <li>helpdesk@company.com</li>
                 <li>Office Hours: Monday-Friday 7-5</li>
             </ul>    
         </footer>
--- hci-program-2/src/data/tickets.js
+++ hci-program-2/src/data/tickets.js
@@ -1,18 +1,18 @@
 export const tickets =[
     {
         "id": 1,
-        "subject": "Email   certificate expired",
-        "type": "Client response pending",
-        "name": "2020-09-23",
+        "subject": "PC wont boot",
+        "type": "Incident",
+        "name": "Jordan Booth",
         "phone": "402-999-2222",
         "date": "9/29/2022"
     },
     {
         "id": 2,
-        "subject": "Email   certificate expired",
-        "type": "Client response pending",
-        "name": "2020-09-23",
+        "subject": "Need temporary laptop",
+        "type": "Request",
+        "name": "Jordan Booth",
         "phone": "402-999-2222",
-        "date": "9/29/2022"
+        "date": "12/3/2022"
     }
 ]
