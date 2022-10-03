324a25a2a32efb2b09f7b920d05bf0a3fb596fec
jacarp <91710233+jacarp@users.noreply.github.com>
2022-10-02 19:08:03 -0500

    Answered last 2 project questions in App.js

--- hci-program-2/src/App.js
+++ hci-program-2/src/App.js
@@ -7,11 +7,16 @@ import InputForm from './pages/input_form';
 import ViewForm from './pages/view_form';
 import Footer from './components/Footer';
 
-// Questions on Canvas 
-//1. Who is our audience? The employees at the company that is using the ticket system. 
-//2. What are the goals for the user? They want to request help with their device or various devices.
-//3. How did we understand and document context and goals?
-//4. Can We/Did We use any of the cognitive/behavior patterns discussed in the textbook? 
+/*Questions on Canvas 
+1. Who is our audience? 
+    The employees at the company that is using the ticket system. 
+2. What are the goals for the user? 
+    They want to request help with their device or various devices.
+3. How did we understand and document context and goals?
+    We assigned certain tasks to each person and put inline comments describing what each part does.
+4. Can We/Did We use any of the cognitive/behavior patterns discussed in the textbook?
+    We made the website look nice and made it so the user only had to enter a few things to request tech help.
+*/
 
 function App() {
 return (
